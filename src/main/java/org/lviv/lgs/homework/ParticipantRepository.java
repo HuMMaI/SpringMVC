98dd3da21f45beebe1fb01299b6ec4827c53def7
hummai <shining.melon2015@gmail.com>
2020-04-17 15:34:24 +0300

    Added participant service

--- src/main/java/org/lviv/lgs/homework/ParticipantRepository.java
+++ src/main/java/org/lviv/lgs/homework/ParticipantRepository.java
@@ -1,7 +1,9 @@
 package org.lviv.lgs.homework;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ParticipantRepository extends JpaRepository<Participant, Integer> {
 
 }
---
+++ src/main/java/org/lviv/lgs/homework/ParticipantService.java
@@ -0,0 +1,40 @@
+package org.lviv.lgs.homework;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ParticipantService {
+    private ParticipantRepository participantRepository;
+
+    @Autowired
+    public ParticipantService(ParticipantRepository participantRepository) {
+        this.participantRepository = participantRepository;
+    }
+
+    public void save(Participant participant){
+        participantRepository.save(participant);
+    }
+
+    public Participant findById(int id){
+        Optional<Participant> participantMaybe = participantRepository.findById(id);
+
+        if (participantMaybe.isPresent()){
+            return participantMaybe.get();
+        } else {
+            String message = String.format("Can't find participant with id = %d", id);
+            throw new RuntimeException(message);
+        }
+    }
+
+    public List<Participant> findAll(){
+        return participantRepository.findAll();
+    }
+
+    public void deleteById(int id){
+        participantRepository.deleteById(id);
+    }
+}
