ce2a2eb2eeb3130aabf27319a41ab03ff8f0c964
hummai <shining.melon2015@gmail.com>
2020-04-21 18:22:03 +0300

    Added response status

--- src/main/java/org/lviv/lgs/homework/controllers/AddPageRestController.java
+++ src/main/java/org/lviv/lgs/homework/controllers/AddPageRestController.java
@@ -1,11 +1,14 @@
 package org.lviv.lgs.homework.controllers;
 
-import org.lviv.lgs.homework.Participant;
-import org.lviv.lgs.homework.ParticipantService;
+import org.lviv.lgs.homework.entities.Participant;
+import org.lviv.lgs.homework.services.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 @RestController
 public class AddPageRestController {
@@ -13,7 +16,15 @@ public class AddPageRestController {
     private ParticipantService participantService;
 
     @PostMapping("/api/add")
-    public void addParticipant(@ModelAttribute Participant participant){
-        participantService.save(participant);
+    public void addParticipant(@ModelAttribute Participant participant, HttpServletResponse responce){
+        if (participant.getCoverId().equals("")){
+            participant.setCoverId(null);
+        }
+
+        Optional<Participant> save = participantService.save(participant);
+
+        if (save.isPresent()){
+            responce.setStatus(HttpServletResponse.SC_OK);
+        }
     }
 }
